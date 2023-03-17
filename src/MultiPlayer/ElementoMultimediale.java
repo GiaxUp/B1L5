8d3aba6314ba46a89b72cd810ca94042f087b5bf
Giacomo <giax.95@live.it>
2023-03-17 16:17:02 +0100

    Media player start stuff.

---
+++ src/MultiPlayer/ElementoMultimediale.java
@@ -0,0 +1,133 @@
+package MultiPlayer;
+
+abstract class ElementoMultimediale {
+	protected String titolo;
+
+	public ElementoMultimediale(String titolo) {
+		this.titolo = titolo;
+	}
+
+	public abstract boolean isRiproducibile();
+
+	public abstract void play();
+
+	public abstract void show();
+}
+
+class Immagine extends ElementoMultimediale {
+	private int luminosita;
+
+	public Immagine(String titolo, int luminosita) {
+		super(titolo);
+		this.luminosita = luminosita;
+	}
+
+	public boolean isRiproducibile() {
+		return false;
+	}
+
+	public void play() {
+		throw new UnsupportedOperationException("Un'immagine non è riproducibile");
+	}
+
+	public void show() {
+		System.out.print(titolo + " ");
+		for (int i = 0; i < luminosita; i++) {
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+}
+
+	class RegistrazioneAudio extends ElementoMultimediale {
+		private int durata;
+		private int volume;
+
+		public RegistrazioneAudio(String titolo, int durata, int volume) {
+			super(titolo);
+			this.durata = durata;
+			this.volume = volume;
+		}
+
+		public boolean isRiproducibile() {
+			return true;
+		}
+
+		public void play() {
+			for (int i = 0; i < durata; i++) {
+				System.out.print(titolo + " ");
+				for (int j = 0; j < volume; j++) {
+					System.out.print("!");
+				}
+				System.out.println();
+			}
+		}
+
+		public void show() {
+			throw new UnsupportedOperationException("Una registrazione audio non è un'immagine");
+		}
+
+		public void abbassaVolume() {
+			if (volume > 0) {
+				volume--;
+			}
+		}
+
+		public void alzaVolume() {
+			volume++;
+		}
+	}
+
+	class Video extends ElementoMultimediale {
+		private int durata;
+		private int volume;
+		private int luminosita;
+
+		public Video(String titolo, int durata, int volume, int luminosita) {
+			super(titolo);
+			this.durata = durata;
+			this.volume = volume;
+			this.luminosita = luminosita;
+		}
+
+		public boolean isRiproducibile() {
+			return true;
+		}
+
+		public void play() {
+			for (int i = 0; i < durata; i++) {
+				System.out.print(titolo + " ");
+				for (int j = 0; j < volume; j++) {
+					System.out.print("!");
+				}
+				for (int j = 0; j < luminosita; j++) {
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+		}
+
+		public void show() {
+			throw new UnsupportedOperationException("Un video non è un'immagine statica");
+		}
+
+		public void abbassaVolume() {
+			if (volume > 0) {
+				volume--;
+			}
+		}
+
+		public void alzaVolume() {
+			volume++;
+		}
+
+		public void diminuisciLuminosita() {
+			if (luminosita > 0) {
+				luminosita--;
+			}
+		}
+
+		public void aumentaLuminosita() {
+			luminosita++;
+		}
+	}
---
+++ src/MultiPlayer/MultimediaPlayer.java
@@ -0,0 +1,59 @@
+package MultiPlayer;
+
+import java.util.Scanner;
+
+public class MultimediaPlayer {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		ElementoMultimediale[] elements = new ElementoMultimediale[5];
+
+		// Crea 5 elementi da tastiera e inseriscili nell'array
+		for (int i = 0; i < 5; i++) {
+			System.out.print("Inserisci il tipo di elemento multimediale (1 = Immagine, 2 = Audio, 3 = Video): ");
+			int type = scanner.nextInt();
+			scanner.nextLine(); // consuma il carattere di fine linea
+			System.out.print("Inserisci il titolo: ");
+			String title = scanner.nextLine();
+			System.out.print("Inserisci la durata in secondi: ");
+			int duration = scanner.nextInt();
+			scanner.nextLine(); // consuma il carattere di fine linea
+
+            ElementoMultimediale element;
+            if (type == 1) {
+                System.out.print("Inserisci la luminosità: ");
+                int brightness = scanner.nextInt();
+                element = new Immagine(title, brightness);
+            } else if (type == 2) {
+                System.out.print("Inserisci il volume: ");
+                int volume = scanner.nextInt();
+                element = new RegistrazioneAudio(title, duration, volume);
+            } else if (type == 3) {
+                System.out.print("Inserisci il volume: ");
+                int volume = scanner.nextInt();
+                System.out.print("Inserisci la luminosità: ");
+                int brightness = scanner.nextInt();
+                element = new Video(title, duration, volume, brightness);
+            } else {
+                System.out.println("Tipo di elemento non valido, riprova.");
+                i--;
+                continue;
+            }
+            elements[i] = element;
+		}
+
+		// Chiedi all'utente quale elemento eseguire cliccando 1-5, esci cliccando 0
+		int choice;
+		do {
+			System.out.print("Quale elemento eseguire? (1-5 per selezionare, 0 per uscire): ");
+			choice = scanner.nextInt();
+			if (choice >= 1 && choice <= 5) {
+				ElementoMultimediale element = elements[choice - 1];
+				if (element.isRiproducibile()) {
+					element.play();
+				} else {
+					element.show();
+				}
+			}
+		} while (choice != 0);
+	}
+}
